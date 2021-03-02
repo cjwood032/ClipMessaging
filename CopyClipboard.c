7dd7b6cd03d319291e563514e908faab53b400e8
cjwood032 <cjwood032@gmail.com>
2021-03-02 15:29:11 -0500

    copy to clipboard appends

--- CopyClipboard.c
+++ CopyClipboard.c
@@ -4,22 +4,28 @@
 #include <Windows.h>
 #include <winuser.h>
 //Copy this I guess?
+/*
+here is a
+multiline example
+to test
+*/
 int main()
 {
-    char output[20]="did not work";
+    char output[20]="did not work ";
 
     OpenClipboard(0);
     HGLOBAL hglb;
     hglb =GetClipboardData(CF_TEXT);
     if(hglb!=NULL)
     {
-        strcpy(output,(char*)hglb);
+        //printf("the copied data\n%s\n",(char*)hglb);
+        strcat(output,(char*)hglb);
     }
     else
     {
         printf("The clipboard was empty\n");
     }
-    printf("%s",output);
+    printf("the final output\n%s\n",output);
     CloseClipboard();
     return 0;
 }
